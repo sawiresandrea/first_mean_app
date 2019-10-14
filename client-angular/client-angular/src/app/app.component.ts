dbd2eadcbbe64178fbb572643b4ee97f08095bc5
sawiresandrea <sawires.andrea@iisgalvanimi.edu.it>
2019-10-14 09:41:17 +0000

    exercise completed

---
+++ client-angular/client-angular/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'; //HTTP CLIENT
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit{
+  results: string[];
+  private BASE_URL:string = '/api';
+    // Inject HttpClient into your component or service.
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+      // Make the HTTP request:
+      this.http.get('https://3000-fb37bc9b-a57c-4ebe-adff-24e980bd3bcf.ws-eu0.gitpod.io/api').subscribe(data => {
+        // Read the result field from the JSON response.
+        this.results = data['results'];
+      });
+    }
+}
